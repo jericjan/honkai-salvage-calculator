e2b20dd8d966337810f01a5457588117cb57a48c
jericjan <cantilfrederick@gmail.com>
2022-08-26 17:47:46 +0800

    UI update: CustomTkinter uses StringVars instead of nametowidget() + configure() slight stamina fix and update

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 "The Main File"
 
 import re
-from tkinter import Label, Tk, Frame, filedialog, Button
+
+# from tkinter import Label, Tk, Frame, filedialog, Button
+from tkinter import filedialog, StringVar, TclError
 from tkinter.messagebox import showinfo, askyesno
 from threading import Thread
 import sys
@@ -9,6 +11,13 @@ import os
 import time
 import math
 from datetime import datetime, timedelta
+from customtkinter import (
+    CTkLabel as Label,
+    CTk as Tk,
+    CTkFrame as Frame,
+    CTkButton as Button,
+)
+import customtkinter
 
 import cv2
 import numpy as np
@@ -16,8 +25,10 @@ from PIL import ImageGrab, Image
 import pyautogui as pg
 from pytesseract import pytesseract
 from win32gui import FindWindow, GetWindowRect
+from pywintypes import error as pywinerror
 
-
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
 
 img_dict = {
     "phase_shifters": "Phase Shifters",
@@ -48,20 +59,25 @@ if not os.path.exists(PATH_TO_TESSERACT):
         sys.exit()
 pytesseract.tesseract_cmd = PATH_TO_TESSERACT
 window = Tk()
-info = Frame(window, name="info")
-info.pack(side="top", fill="x")
-solutions_frame = Frame(window, name="solutions")
-solutions_frame.pack(side="top", fill="x", pady=10)
+window.geometry("436x331")
+frame1 = Frame(window, name="frame1")
+frame1.pack(side="top", pady=20)
+info = Frame(frame1, name="info")
+info.pack(side="left")
+solutions_frame = Frame(frame1, name="solutions")
+solutions_frame.pack(side="left", padx=20)
+frame2 = Frame(window, name="frame2")
+frame2.pack(side="top")
 
 
 def get_window_size():
     "Gets the game's window size"
     try:
         window_rect = GetWindowRect(FindWindow(None, "Honkai Impact 3"))
-    except:
+    except pywinerror:
         try:
             window_rect = GetWindowRect(FindWindow(None, "Honkai Impact 3rd"))
-        except:
+        except pywinerror:
             showinfo(
                 title="Game not running",
                 message="Can't find a running HI3 game. Exiting...",
@@ -76,44 +92,51 @@ def get_window_size():
 if get_window_size() != (1286, 749):
     showinfo(
         title="Hey!",
-        message="The game needs to be running in 1280x720 windowed mode for this program to work. Sorry.",
+        message="The game needs to be running in 1280x720 windowed mode for this program to work. "
+        "Sorry.",
     )
     sys.exit()
 
 
 def make_gui():
     "makes the GUI for the # of items, and items needed to be sold"
-    
-    scan_mats_button = Button(master=info,text="Scan Mats",command=lambda: run_in_thread(scanner))
-    scan_mats_button.pack()    
-    
+
+    scan_mats_button = Button(
+        master=info, text="Scan Mats", command=lambda: run_in_thread(scanner)
+    )
+    scan_mats_button.pack(pady=10)
+
     for name, clean_name in img_dict.items():
-        lbl = Label(master=info, name=name, text=f"{clean_name}: [Not detected]")
+        var = StringVar(window, f"{clean_name}: [Not detected]", f"info_{name}")
+        lbl = Label(master=info, textvariable=var)
         lbl.pack()
 
-    for idx, thing in enumerate(sellables.items()):
-        name, clean_name = thing
-        chunk_frame = Frame(solutions_frame, name=str(idx))
-        chunk_frame.pack(side="top")
-        lbl = Label(master=chunk_frame, text="You should sell:")
-        lbl.pack(side="left")
-        lbl = Label(master=chunk_frame, name=name, text=f"?? {clean_name}")
-        lbl.pack(side="left")
-    stam_button = Button(master=window,text="Calculate Stamina",command=lambda: run_in_thread(stamina))
-    stam_button.pack()
-    stam_text = Label(master=window,name="stam_text",text="")
-    stam_text.pack()
+    lbl = Label(master=solutions_frame, text="You should sell:")
+    lbl.pack(side="top")
+    for name, clean_name in sellables.items():
+        var = StringVar(window, f"?? {clean_name}", f"solutions_{name}")
+        lbl = Label(master=solutions_frame, textvariable=var)
+        lbl.pack(side="top")
+    stam_button = Button(
+        master=frame2, text="Calculate Stamina", command=lambda: run_in_thread(stamina)
+    )
+    stam_button.pack(side="top", pady=10)
+    var = StringVar(window, "", "stam_text")
+    stam_text = Label(master=frame2, textvariable=var, name="stam_text")
+    stam_text.pack_forget()
     window.title("Salvage calculator")
     calc_thread = Thread(target=calculate)
     calc_thread.daemon = True
-    calc_thread.start()    
+    calc_thread.start()
+
 
 def stamina():
+    "calculates when stamina reaches max"
     screen = np.array(ImageGrab.grab())
     screen = cv2.cvtColor(src=screen, code=cv2.COLOR_BGR2RGB)
-    thing = pg.locate(f"images/stamina.png", screen, confidence=0.8)
+    thing = pg.locate("images/stamina.png", screen, confidence=0.8)
     if thing:
-        start_row = thing.top +10
+        start_row = thing.top + 10
         end_row = thing.top + thing.height
         start_column = thing.left + thing.width
         end_column = start_column + 120
@@ -124,25 +147,36 @@ def stamina():
         cropped = make_border(cropped, white, 5)
         cropped = make_border(cropped, red, 3)
         cropped = make_border(cropped, white, 2)
-        cropped = make_border(cropped, red, 1)    
+        cropped = make_border(cropped, red, 1)
         text = pytesseract.image_to_string(cropped)
-        label = window.nametowidget(".stam_text")
-        if re.findall(r"\d{1,3}/\d{1,3}",text):
-            curr_stam, max_stam = [int(x) for x in text.strip().split('/')]
+
+        if re.findall(r"\d{1,3}/\d{1,3}", text):
+            curr_stam, max_stam = [
+                int(x) for x in re.findall(r"\d{1,3}/\d{1,3}", text)[0].split("/")
+            ]
             full_hours = (max_stam - curr_stam) / 10
             hours = math.floor(full_hours)
             if not (full_hours % 1) == 0:
                 minutes = (full_hours - hours) * 60
             else:
                 minutes = 0
-            time_when_max = (datetime.now() + timedelta(hours=hours,minutes=minutes)).strftime('%b %d - %I:%M %p')
-            label.configure(text=f"{curr_stam}/{max_stam}\n" \
-            f"Full stamina in {hours}H:{minutes:.0f}M\n" \
-            f"{time_when_max}")
-        else:        
-            label.configure(text="I scanned it wrong.")
-    
-    
+            time_when_max = (
+                datetime.now() + timedelta(hours=hours, minutes=minutes)
+            ).strftime("%b %d - %I:%M %p")
+            window.setvar(
+                "stam_text",
+                f"{curr_stam}/{max_stam}\n"
+                f"Full stamina in {hours}H:{minutes:.0f}M\n"
+                f"{time_when_max}",
+            )
+        else:
+            window.setvar("stam_text", "I scanned it wrong.")
+        window.nametowidget(".frame2.!ctklabel").pack()
+    else:
+        window.setvar("stam_text", "I couldn't find the stamina logo.")
+        window.nametowidget(".frame2.!ctklabel").pack()
+
+
 def calculate():
     "calculates the # of items needed to be sold"
     shifter_count = get_value("phase_shifters")
@@ -150,16 +184,16 @@ def calculate():
     if shifter_count and will_count:
         sell_will = will_count - (shifter_count / 3)
         sell_will = max(sell_will, 0)
-        label = window.nametowidget(".solutions.0.twin_sakura_will")
-        label.configure(text=f"{sell_will:.2f} Twin Sakura Will(s)")
+        window.setvar(
+            "solutions_twin_sakura_will", f"{sell_will:.2f} Twin Sakura Will(s)"
+        )
 
     torus_count = get_value("torus")
     nano_count = get_value("nano")
     if torus_count and nano_count:
         sell_nano = nano_count - ((torus_count * 45) / 100)
         sell_nano = max(sell_nano, 0)
-        label = window.nametowidget(".solutions.1.nano")
-        label.configure(text=f"{sell_nano:.2f} Nanoceramic(s)")
+        window.setvar("solutions_nano", f"{sell_nano:.2f} Nanoceramic(s)")
 
     skill_mats_count = get_value("skill_mats")
     if skill_mats_count:
@@ -167,8 +201,10 @@ def calculate():
         sell_mats_2 = skill_mats_count - 30
         sell_mats_1 = max(sell_mats_1, 0)
         sell_mats_2 = max(sell_mats_2, 0)
-        label = window.nametowidget(".solutions.2.skill_mats")
-        label.configure(text=f"{sell_mats_1}-{sell_mats_2} Advanced Skill Material(s)")
+        window.setvar(
+            "solutions_skill_mats",
+            f"{sell_mats_1}-{sell_mats_2} Advanced Skill Material(s)",
+        )
 
     window.after(1000, calculate)
 
@@ -176,11 +212,10 @@ def calculate():
 def get_value(name):
     "gets the label item's value"
     try:
-        label = window.nametowidget(f".info.{name}")
-    except KeyError:
+        label = window.getvar(f"info_{name}")
+    except TclError:
         return False
-    text = label["text"]
-    value = re.findall(r"\d+", text)
+    value = re.findall(r"\d+", label)
     if value:
         return int(value[0])
     return False
@@ -261,17 +296,15 @@ def thing_locater(screen, filename, name):
             text = text.strip()
             if re.search(r"x +\d+|x\d+|\d+", text):
                 text = re.findall(r"\d+", text)[0]
-                try:
-                    label = window.nametowidget(f".info.{filename}")
-                except KeyError:
-                    print(f"'.{filename}' not found")
-                    return
-                print(f"{name} - {text.strip()}")
-                label.configure(text=f"{name}: {text}")
+                window.setvar(f"info_{filename}", f"{name}: {text}")
+
 
 def run_in_thread(func):
+    "runs a function in a thread"
     le_thread = Thread(target=func)
     le_thread.start()
+
+
 def scanner():
     "calls thing_locater in threads"
     start_time = time.time()
@@ -288,12 +321,11 @@ def scanner():
         thread.start()
 
     print(f"---------------END - {(time.time() - start_time):.2f}")
-        
-        
 
 
 gui_thread = Thread(target=make_gui)
 gui_thread.daemon = True
 gui_thread.start()
+
 
 window.mainloop()
